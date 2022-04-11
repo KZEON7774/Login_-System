a141bde7d472bb7c98ad9a40d1b3495fd15c251e
KZEON7774 <67939079+KZEON7774@users.noreply.github.com>
2022-04-11 13:17:25 +0530

    Add files via upload

---
+++ Login_System1/welcome.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="icon" href="icons/2.png">
+</head>
+<body>
+    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
